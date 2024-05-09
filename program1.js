1785019ec5ead47f390f2ab1fc133664198fba6b
Vaishnavi Kale <169052118+Vaishnavi2500@users.noreply.github.com>
2024-05-09 13:25:24 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,10 +1,15 @@
-const getTotalIsles = function (grid) {
+const arr=["W","L","L","W","L"];
+const arr1=["L","L","W","W","W"];
+const arr2=["L","L","L","W","W"];
 
+    const getTotalIsles = function (grid) {
 
-    // write your code here
-
-  };
-  
+        return arr.foreach;
+        return arr1;
+        return arr2.foreach;
+     };
+  grid();
+return 1;
   module.exports = getTotalIsles;
 
 
